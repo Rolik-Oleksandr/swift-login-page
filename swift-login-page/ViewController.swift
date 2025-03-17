6f69d3e19b0b2e68b688a9c3596d18967b597b88
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-03-18 00:16:42 +0200

    Initial Commit

---
+++ swift-login-page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-login-page
+//
+//  Created by Oleksandr Rolik on 18.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swift-login-pageTests/swift_login_pageTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_login_pageTests.swift
+//  swift-login-pageTests
+//
+//  Created by Oleksandr Rolik on 18.03.2025.
+//
+
+import Testing
+@testable import swift_login_page
+
+struct swift_login_pageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
