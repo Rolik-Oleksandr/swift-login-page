843916044a5496041403053c6c531fa533180938
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-03-20 17:11:05 +0200

    reformate

---
+++ swift-login-page/Controllers/AuthViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let loginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
