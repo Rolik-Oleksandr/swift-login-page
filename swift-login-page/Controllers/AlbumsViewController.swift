b275cd370f08dfc5518518725f0ebcf977caf78d
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-04-05 21:38:57 +0300

    get connection to API

--- swift-login-page/Controllers/AlbumsViewController.swift
+++ swift-login-page/Controllers/AlbumsViewController.swift
@@ -12,6 +12,9 @@ class AlbumsViewController: UIViewController {
     
     private let searchController = UISearchController (searchResultsController: nil)
     
+    var albums = [Album]()
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -54,15 +57,35 @@ class AlbumsViewController: UIViewController {
         userInfoViewController.modalPresentationStyle = .fullScreen
         navigationController?.pushViewController(userInfoViewController, animated: true)
     }
+    private func fetchAlbums(albumName: String) {
+        
+        let urlString = "https://itunes.apple.com/search?term=\(albumName)&entity=album&attriute=albumTerm"
+        
+        NetworkDataFetch.shared.fetchAlbum(urlString: urlString) { [weak self] albumModel, error in
+            
+            if error == nil {
+                
+                guard let albumModel = albumModel else { return }
+                self?.albums = albumModel.results
+                print(self?.albums)
+                self?.tableView.reloadData()
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    
 }
 //MARK: - Extension
 extension AlbumsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        albums.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! AlbumsTableViewCell
+        let album = albums[indexPath.row]
+        cell.configureAlbumCell(album: album)
         
         return cell
     }
@@ -79,7 +102,12 @@ extension AlbumsViewController: UITableViewDelegate {
 extension AlbumsViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
-        print(searchBar)
+        if searchText != "" {
+            timer?.invalidate()
+            timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { [weak self] _ in
+                self?.fetchAlbums(albumName: searchText)
+            })
+        }
     }
 }
 
