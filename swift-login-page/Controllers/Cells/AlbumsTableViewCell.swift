fca53049e20068ddb1dc294f44105384880aa2c5
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-04-07 13:27:02 +0300

    get ablum logo from api

--- swift-login-page/Controllers/Cells/AlbumsTableViewCell.swift
+++ swift-login-page/Controllers/Cells/AlbumsTableViewCell.swift
@@ -66,6 +66,30 @@ class AlbumsTableViewCell: UITableViewCell {
                                 distibution: .equalCentering)
         self.addSubview(stackView)
     }
+    
+    func configureAlbumCell(album: Album) {
+        
+        if let urlString = album.artworkUrl100 {
+            NetworkRequest.shared.requestData(urlString: urlString) { result in
+                switch result {
+                    
+                case .success(let data):
+                    let image = UIImage(data: data)
+                    self.albumLogo.image = image 
+                case .failure(let error):
+                    self.albumLogo.image = nil
+                    print("No image + \(error.localizedDescription)")
+                }
+            }
+        } else {
+            albumLogo.image = nil
+        }
+        
+        albumNameLabel.text = album.collectionName
+        artistNameLabel.text = album.artistName
+        trackCountLabel.text = "\(album.trackCount) tracks"
+        
+    }
 
     private func setConstraints() {
 
