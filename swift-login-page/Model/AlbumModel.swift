b5999822880bfe25049734253ee7a9060db5c928
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-04-28 23:35:57 +0300

    models

---
+++ swift-login-page/Model/AlbumModel.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct AlbumModel: Decodable, Equatable {
+    let results: [Album]
+}
+
+struct Album: Decodable, Equatable {
+    let artistName: String
+    let collectionName: String
+    let artworkUrl100: String?
+    let trackCount: Int
+    let releaseDate: String
+}
